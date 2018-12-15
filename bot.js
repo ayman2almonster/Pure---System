c8488b39cd6d544236fa9474159ead22becbb3f5
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 19:26:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2118,7 +2118,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`*help |the bot by Carezma | Vettel`,"http://twitch.tv/Death Shop")
+client.user.setGame(`*help |the bot byPure Server | Vettel`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
